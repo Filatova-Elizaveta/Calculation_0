e6632e92fe29e1c18383d1883e6ea716ba213b1a
Filatova-Elizaveta <47223480+Filatova-Elizaveta@users.noreply.github.com>
2024-12-22 14:03:13 +0300

    Create errors.go

---
+++ pkg/calc/errors.go
@@ -0,0 +1,10 @@
+package calc
+
+import "errors"
+
+var (
+	ErrDivisionByZero      = errors.New("division by zero")
+	ErrInvalidExpression   = errors.New("invalid expression")
+	ErrSomethingWentWrong  = errors.New("something went wrong")
+	ErrExpressionNotString = errors.New("expression not a string")
+)
